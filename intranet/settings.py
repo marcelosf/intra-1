dd7a4ab978bf4da0d3553aa7b8c55b98b516f2dc
Marcelo Schneider <schneider.fei@gmail.com>
2019-10-25 10:39:07 -0300

    Adiciona tema no formulário de acesso - issue#19
      - Alteração do template do formulário

--- intranet/settings.py
+++ intranet/settings.py
@@ -151,6 +151,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 #Email configuration
 EMAIL_BACKEND = config('EMAIL_BACKEND')
