b3ab73092d2250c0e4c905d2a3b17eecd01e1e99
Marcelo Schneider <schneider.fei@gmail.com>
2019-12-20 10:36:55 -0300

    Valida campos do formulário - Fixes #84

--- intranet/access/forms/forms.py
+++ intranet/access/forms/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.core.exceptions import ValidationError
-from intranet.access.validators import validate_phone
+from intranet.access import validators
 from intranet.access.forms.form_choices import DOCS, ANSWERABLE, STATUS
 
 
@@ -16,18 +16,16 @@ class AccessForm(forms.Form):
     name = forms.CharField(label='Nome')
     job = forms.CharField(label='Cargo')
     email = forms.EmailField(label='E-mail')
-    phone = forms.CharField(label='Telefone', validators=[validate_phone])
+    phone = forms.CharField(label='Telefone', validators=[validators.validate_phone])
     doc_type = forms.ChoiceField(label='Documento', choices=DOCS)
     doc_number = forms.CharField(label='Número do documento')
     answerable = forms.ChoiceField(label='Responsável', choices=ANSWERABLE)
     observation = forms.CharField(label='Observação', widget=forms.Textarea(attrs={'class': 'materialize-textarea'}))
 
     def clean(self):
-        period_to = self.cleaned_data.get('period_to')
-        period_from = self.cleaned_data.get('period_from')
-
-        if (period_from and period_to) and (period_from > period_to):
-            raise ValidationError('A Data de início deve ser menor do que a Data de término')
+        cleaned_data = super().clean()
+        validators.validate_period(cleaned_data.get('period_from'), cleaned_data.get('period_to'))
+        validators.validate_doc_number(self, cleaned_data.get('doc_type'), cleaned_data.get('doc_number'))
         return self.cleaned_data
 
     def clean_name(self):
--- intranet/access/validators.py
+++ intranet/access/validators.py
@@ -1,4 +1,5 @@
 from django.core.exceptions import ValidationError
+from validate_docbr import CPF
 
 def validate_phone(value):
     if not value.isdigit():
@@ -6,3 +7,25 @@ def validate_phone(value):
 
     if len(value) > 11:
         raise ValidationError('Telefone deve ter no máximo 11 digitos', 'length')
+
+
+def validate_period(period_from, period_to):
+    if (period_from and period_to) and (period_from > period_to):
+        raise ValidationError('A Data de início deve ser menor do que a Data de término')
+
+
+def validate_doc_number(form, doc_type, doc_number):
+    if doc_type == 'RG' and len(doc_number) != 9:
+        error = ValidationError('RG deve ter 9 digitos', 'length')
+        form.add_error('doc_number', error)
+
+    if doc_type == 'CPF' and not _cpf_is_valid(doc_number):
+        error = ValidationError('CPF é invalido', 'cpf')
+        form.add_error('doc_number', error)
+
+
+def _cpf_is_valid(cpf_number):
+    cpf = CPF()
+    return cpf.validate(cpf_number)
+
+    
