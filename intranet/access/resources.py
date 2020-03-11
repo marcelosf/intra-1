6acf77ca9b3dcf3e9a57978ed18d8e3e3b54a54a
Marcelo Schneider <schneider.fei@gmail.com>
2020-03-11 18:18:38 -0300

    Adiciona configuração para o path do resource - issue #87

--- intranet/access/resources.py
+++ intranet/access/resources.py
@@ -4,14 +4,19 @@ import requests
 from urllib.parse import urljoin
 
 
+GET_ALUNOS_RESOURCE = {'replicated': 'students'}
+
+
 def get_alunos(query=None):
     url_base = settings.ALUNOS_RESOURCES
     if not query:
         resp = requests.get(url=url_base)
         return resp
     url_query = ''
-    for qs in query.keys():
-        url_query = '/'.join([url_query, qs, query[qs]])
+    search_keys = dict(GET_ALUNOS_RESOURCE, **query)
+    for qs in search_keys.keys():
+        if search_keys[qs]:
+            url_query = '/'.join([url_query, qs, search_keys[qs]])
     url = urljoin(base=url_base, url=url_query)
     resp = requests.get(url=url)
     return resp
