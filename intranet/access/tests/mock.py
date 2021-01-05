0b9cc442eebf0784d44025db63a74cfa86d30dce
Marcelo Schneider <schneider.fei@gmail.com>
2021-01-05 17:46:27 -0300

    Cria dados para teste do módulo api_resources #87
    No módulo mock foram adicionados dados de teste para o módulo
    api_resources. Os dados simulam uma resposta para a requisição
    enviada a api. Além disso, foi removido antigos dados de teste
    pois não mais representava a estrutura dos dados retornada pela
    api.

--- intranet/access/tests/mock.py
+++ intranet/access/tests/mock.py
@@ -1,3 +1,8 @@
+import json
+import httpretty
+import functools
+
+
 from ..models import Access
 
 
@@ -27,3 +32,62 @@ def make_access():
     data = make_data()
     Access.objects.create(**data)
     return data
+
+
+base_url = 'http://api.iag.usp.br/'
+
+
+alunos_by_tipo_vinculo = [
+    {
+        'byTipvin': [
+            {
+                "codpes": "1111111",
+                "tipvinext": "Aluno de Cultura e Extensão",
+                "sitatl": "A",
+                "codundclg": "14",
+                "nompes": "Merlyn Steves",
+                "codema": "mll@gmail.com"
+            }
+        ]
+    }
+]
+
+
+token_payload = {
+    'access_token': '87FXExQqaF2NUL9byWcFAQchxUlvoXL', 'token_type': 'bearer'}
+
+
+api_list = [
+    {
+        'method': 'POST',
+        'uri': base_url + 'token/',
+        'headers': {'Content-Type': 'application/json'},
+        'body': json.dumps(token_payload)
+    },
+    {
+        'method': 'GET',
+        'uri': base_url + 'localizapessoa?query{byTipvin(tipvin: "ALUNOPOS"){codpes,nompes,tipvinext,codema,sitatl}}',
+        'headers': {'Content-Type': 'application/json'},
+        'body': json.dumps(alunos_by_tipo_vinculo)
+    }
+]
+
+
+def register_api_uri():
+    for api in api_list:
+        httpretty.register_uri(
+            getattr(httpretty, api.get('method')),
+            headers=api.get('headers'),
+            uri=api.get('uri'),
+            body=api.get('body')
+        )
+
+
+def mock_api(fn):
+    @functools.wraps(fn)
+    @httpretty.activate
+    def wrapper(*args, **kwargs):
+        register_api_uri()
+        response = fn(*args, **kwargs)
+        return response
+    return wrapper
