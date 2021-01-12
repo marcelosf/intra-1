9e1cc7e3b82ef4e546013bd77b750b081b6cea5c
Marcelo Schneider <schneider.fei@gmail.com>
2021-01-12 19:06:09 -0300

    Testa classe ResourceBy #87
    Testa a clase ResourceBy

--- intranet/access/tests/test_access_api_resources.py
+++ intranet/access/tests/test_access_api_resources.py
@@ -1,7 +1,8 @@
 import requests
 from django.test import TestCase
+from django.conf import settings
 
-from ..api_resources import Resource
+from ..api_resources import Resource, ResourceBy
 from . import mock
 
 
@@ -38,3 +39,31 @@ class ResourceTest(TestCase):
         bearer = 'Bearer {}'.format(mock.token_payload.get('access_token'))
         expected = {'Authorization': bearer}
         self.assertEqual(expected, self.obj.bearer)
+
+
+class ResourceByTest(TestCase):
+    def setUp(self):
+        self.obj = ResourceBy()
+
+    def test_instance(self):
+        self.assertIsInstance(self.obj, Resource)
+
+    @mock.mock_api
+    def test_get_by_name(self):
+        self.obj.set_headers()
+        self.obj.get_token()
+        self.obj.get_bearer()
+
+        bearer = 'Bearer {}'.format(mock.token_payload.get('access_token'))
+        expected = {'Authorization': bearer}
+        self.assertEqual(expected, self.obj.bearer)
+
+    def test_has_make_query_attribute(self):
+        self.assertTrue(hasattr(self.obj, 'make_query'))
+
+    def test_make_query_response(self):
+        query = '?query={byNompes(nompes: "Gilbert"){codpes,nompes,tipvinext,codema,sitatl}}'
+        expected = getattr(settings, 'RESOURCE_ENDPOINT') + query
+
+        self.obj.make_query('Gilbert')
+        self.assertEqual(expected, self.obj.query)
